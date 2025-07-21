129270575044856f7b4449579ecd0db8cdcff5d8
ovalkin <egorvasilevskiy585@gmail.com>
2025-07-21 12:09:22 +0500

    add category migrations

---
+++ database/migrations/2025_07_21_054424_create_products_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_id')->constrained('product_categories', 'id');
+            $table->string('name');
+            $table->decimal('price');
+            $table->integer('amount');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
---
+++ database/migrations/2025_07_21_061401_create_category_parameter_values_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('category_parameter_values', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('category_parameter_id')->constrained('category_parameters', 'id');
+            $table->foreignId('product_id')->constrained('products', 'id');
+            $table->string('value');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('category_parameter_values');
+    }
+};
