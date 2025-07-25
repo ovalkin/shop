479ada76d8afae8476b1b8a535ad95e103fb63fd
ovalkin <egorvasilevskiy585@gmail.com>
2025-07-25 18:05:40 +0500

    slug unique

--- database/migrations/2025_07_21_054359_create_product_categories_table.php
+++ database/migrations/2025_07_21_054359_create_product_categories_table.php
@@ -15,7 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->text('description');
-            $table->string('slug');
+            $table->string('slug')->unique();
             $table->foreignId('parent_id')->nullable();
             $table->timestamps();
             $table->softDeletes();
